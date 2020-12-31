93d64301acc6e81604910d180871fb08e14d31de
fm-labs <flowmotionlabs@gmail.com>
2020-12-31 04:10:27 +0100

    Updated travisCI script

--- bin/ci/travis.sh
+++ bin/ci/travis.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -ex
 
+WD=`pwd`
+
+echo "WD: ${WD}"
+echo "TRAVIS_REPO_SLUG: ${TRAVIS_REPO_SLUG}"
+echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
 echo "TRAVIS_JOB_ID: ${TRAVIS_JOB_ID}"
 echo "TRAVIS_JOB_NAME: ${TRAVIS_JOB_NAME}"
 echo "TRAVIS_JOB_NUMBER: ${TRAVIS_JOB_NUMBER}"
+echo "TRAVIS_JOB_WEB_URL: ${TRAVIS_JOB_WEB_URL}"
 echo "TRAVIS_BUILD_DIR: ${TRAVIS_BUILD_DIR}"
 echo "TRAVIS_BUILD_ID: ${TRAVIS_BUILD_ID}"
 
@@ -13,7 +19,7 @@ nohup php -S localhost:8855 -t ${TRAVIS_BUILD_DIR}/tests/test_app ${TRAVIS_BUILD
 PHP_SERVER_PID=$!
 
 # Run integration tests
-composer run tests-integration
+composer run test-integration
 SUCCESS=$?
 
 kill -3 $PHP_SERVER_PID
